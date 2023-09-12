3b61dee8a0556eb86338e57b1349e74a86755527
Mina Talaat Zaki <55636029+minatalaat@users.noreply.github.com>
2023-09-12 12:47:13 +0300

    add firebase

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import {useState} from 'react'
+import { fetchToken,onMessageListener } from './firebase';
+import {Toast} from 'react-bootstrap'
+function requestPermission() {
+  Notification.requestPermission().then((permission) => {
+    if (permission === 'granted') {
+    }
+  }
+  )
+}
+
+function App() {
+  const [show,setShow] = useState(false)
+  const [notification,setNotification] = useState({title:'',body:''})
+  const [isTokenFound,setIsTokenFound] = useState(false)
+  const [fcmToken,setFcmToken] = useState('')
+  requestPermission()
+  fetchToken(setIsTokenFound,setFcmToken)
+
+  onMessageListener().then(payload =>{
+    setNotification({
+      title:payload.notification.title,
+      body:payload.notification.body
+    })
+    setShow(true)
+  }).catch(err => console.log('failed'))
+  return (
+    <div className="App">
+      <Toast onClose={()=>setShow(false)} show={show} delay={300}>
+        <Toast.Header>
+          <strong className='mr-auto'>{notification.title}</strong>
+        </Toast.Header>
+        <Toast.Body>
+          {notification.body}
+        </Toast.Body>
+      </Toast>
+      <header className="App-header">
+        {isTokenFound && <h1>notification permission enable</h1>}
+        {isTokenFound && <h6>FCM Token: {fcmToken}</h6>}
+        {!isTokenFound && <h6>Not Permission</h6>}
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,39 @@
+import { initializeApp } from "firebase/app";
+import {getMessaging,getToken,onMessage} from "firebase/messaging"
+
+var firebaseConfig ={  
+    apiKey: "AIzaSyB0IRVwO9dklvHm_cMG-_Qq4e6u6eK8fmw",
+    authDomain: "moj-notification-system.firebaseapp.com",
+    projectId: "moj-notification-system",
+    storageBucket: "moj-notification-system.appspot.com",
+    messagingSenderId: "64185784083",
+    appId: "1:64185784083:web:049f2332110eefb294ece6",
+    measurementId: "G-Q1Q6QJJPQ6"
+}
+const firebaseapp = initializeApp(firebaseConfig)
+const messaging = getMessaging(firebaseapp)
+
+export const fetchToken  = (setTokenFound,setFcmToken) =>{
+    return getToken(messaging, { vapidKey: 'BOCMLONttimv2aPbRAJ_NSeQVuDynTMHRy4N3ZxuuGc4gs-6MRelQDkywWTl9Gu38G8GgMu7hD_RSxz4JxBe9Ic' }).then((currentToken) => {
+        if (currentToken) {
+          console.log(currentToken);
+          setTokenFound(true)
+          setFcmToken(currentToken)
+        } else {
+          // Show permission request UI
+          setTokenFound(false)
+          setFcmToken('')
+          // ...
+        }
+      }).catch((err) => {
+        console.log('An error occurred while retrieving token. ', err);
+        // ...
+      });
+}
+
+export const onMessageListener = () =>
+    new Promise((resolve)=>{
+        onMessage(messaging,(payload)=>{
+            resolve(payload)
+        });
+    });
