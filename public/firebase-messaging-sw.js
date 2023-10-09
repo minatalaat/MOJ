4113c94767793728dc8841e6725f3eb3aa32e7f7
Mina Talaat Zaki <55636029+minatalaat@users.noreply.github.com>
2023-10-09 17:24:13 +0300

    comment firebase config

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,28 +1,28 @@
 
 
 
-import firebase from "firebase/app";
-import "firebase/messaging";
+// import firebase from "firebase/app";
+// import "firebase/messaging";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB0IRVwO9dklvHm_cMG-_Qq4e6u6eK8fmw",
-  authDomain: "moj-notification-system.firebaseapp.com",
-  projectId: "moj-notification-system",
-  storageBucket: "moj-notification-system.appspot.com",
-  messagingSenderId: "64185784083",
-  appId: "1:64185784083:web:049f2332110eefb294ece6",
-  measurementId: "G-Q1Q6QJJPQ6"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB0IRVwO9dklvHm_cMG-_Qq4e6u6eK8fmw",
+//   authDomain: "moj-notification-system.firebaseapp.com",
+//   projectId: "moj-notification-system",
+//   storageBucket: "moj-notification-system.appspot.com",
+//   messagingSenderId: "64185784083",
+//   appId: "1:64185784083:web:049f2332110eefb294ece6",
+//   measurementId: "G-Q1Q6QJJPQ6"
+// };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
 
 
-// Initialize Firebase Cloud Messaging and get a reference to the service
-const messaging = firebase.messaging();
-messaging.onBackgroundMessage(function(payload){
-    console.log(payload);
-    console.log(payload.notification.title);
-    console.log(payload.notification.body);
-    // self.ServiceWorkerRegistration.showNotification()
-  })
+// // Initialize Firebase Cloud Messaging and get a reference to the service
+// const messaging = firebase.messaging();
+// messaging.onBackgroundMessage(function(payload){
+//     console.log(payload);
+//     console.log(payload.notification.title);
+//     console.log(payload.notification.body);
+//     // self.ServiceWorkerRegistration.showNotification()
+//   })
